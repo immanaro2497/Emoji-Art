b9d22216520d0beff3957f1d46ec430355c22e09
immanaro2497 <immanuel2497@gmail.com>
2024-06-26 17:01:26 +0530

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Immanuel on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
